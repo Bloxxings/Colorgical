681032cfbd58f254a99a984d6c29db4e47e37e1a
Guiguithub <guillaume2aix.dly@gmail.com>
2026-02-20 15:34:18 +0100

    noic

---
+++ src/main.py
@@ -0,0 +1,86 @@
+import pygame  # python.exe -m pip install pygame-ce
+from player import PlayerClass
+from terrain_gen import TerrainGenClass
+from cycle import DayNightCycleClass
+
+class GameClass:
+    def __init__(self):
+        pygame.init()
+        pygame.mixer.init()
+        pygame.font.init()
+
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self.SCREEN_WIDTH, self.SCREEN_HEIGHT = self.screen.get_size()
+        pygame.mouse.set_visible(False)
+        
+        self.assets = Assets()
+        self.assets.load_cursor()
+        self.assets.load_heart()
+
+        self.fps_font = pygame.font.SysFont("Arial", 24, bold=True)
+        
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption("Gardening nightmares")
+        self.running = True
+        self.terrain = TerrainGenClass(self.SCREEN_WIDTH, self.SCREEN_HEIGHT)
+        self.player = PlayerClass(self.SCREEN_WIDTH, self.SCREEN_HEIGHT)
+        self.enemy = EnemyClass(100, 100, self.player, self.terrain)
+        self.inventory = InventoryClass(self)
+        self.dayNightCycle = DayNightCycleClass(self.SCREEN_WIDTH, self.SCREEN_HEIGHT)
+        
+
+    def update(self, dividedTime):
+        pressed_keys = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                self.running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1: 
+                if self.inventory.hotbar[self.inventory.selectedHotbarSlot] == "hoe":
+                    self.terrain.modify_tile(pygame.mouse.get_pos(), "farmland")
+                elif self.inventory.hotbar[self.inventory.selectedHotbarSlot] == "sword" and self.player.swingCooldown == 0:
+                    self.player.isSwinging = True
+            elif event.type == pygame.TEXTINPUT:
+                self.inventory.select_hotbar_slot(event.text)
+        
+        self.terrain.move_player(pressed_keys)
+        self.enemy.update()
+        self.dayNightCycle.update(dividedTime)
+        
+
+    def draw(self):
+        self.screen.fill((0, 0, 0))
+        
+        # Entities
+        self.terrain.draw_terrain(self.screen)
+        self.enemy.draw(self.screen)
+        self.player.draw_player(self.screen)
+        self.player.draw_held_tool(self.screen,self.inventory.EveryBlocks,
+                                   self.inventory.hotbar[self.inventory.selectedHotbarSlot])
+        
+        # UI 
+        self.dayNightCycle.draw(self.screen)
+        self.player.draw_player_info(self.screen,self.assets.heart,self.terrain)
+        self.inventory.draw_hotbar(self.screen)
+        
+        self.draw_fps()
+        self.draw_mouse()
+
+    def draw_fps(self):
+        fps_val = int(self.clock.get_fps())
+        fps_surface = self.fps_font.render(f"FPS: {fps_val}", True, (255, 255, 0))
+        self.screen.blit(fps_surface, (20, 20))
+
+    def draw_mouse(self):
+        mousePos = pygame.mouse.get_pos()
+        self.screen.blit(self.assets.cursor, mousePos)
+        
+
+game = GameClass()
+
+while game.running:
+    dividedTime = game.clock.tick(60) / 1000
+    game.update(dividedTime)
+    game.draw()
+    pygame.display.flip()
+
+pygame.quit()
---
+++ src/terrain_gen.py
@@ -0,0 +1,148 @@
+import pygame
+import random
+import math
+from opensimplex import OpenSimplex
+
+from config import Assets
+
+
+def get_random_value(x, y, randomNumber):
+    return ((math.sin(x * 12.9898 + y * 78.233 + randomNumber) * 43758.5453) % 1)
+
+
+class TerrainGenClass:
+    def __init__(terrain, screen_width, screen_height):
+        terrain.SCREEN_WIDTH = screen_width
+        terrain.SCREEN_HEIGHT = screen_height
+        terrain.TILE_SIZE = 40
+        terrain.PLAYER_SPEED = 10
+        terrain.x, terrain.y = 0, 0
+        terrain.tmpNoiseBiomes = OpenSimplex(seed=random.randint(0, 1000000))
+        terrain.tmpNoiseWater = OpenSimplex(seed=random.randint(0, 1000000))
+        terrain.lightOverlay = pygame.Surface((terrain.TILE_SIZE, terrain.TILE_SIZE), pygame.SRCALPHA)
+        terrain.lightOverlay.fill((200, 200, 200, 50))  
+        terrain.mouseHighlightOverlay = pygame.Surface((terrain.TILE_SIZE, terrain.TILE_SIZE), pygame.SRCALPHA)
+        terrain.mouseHighlightOverlay.fill((255, 255, 255, 100)) 
+        terrain.ModifiedTiles = {}
+
+        terrain.assets = Assets(terrain.TILE_SIZE)
+        terrain.assets.load_grass() # Get an image : terrain.assets.GrassList[index]
+        terrain.assets.load_farmland()
+        terrain.assets.load_nature()
+        terrain.assets.load_tree()
+        terrain.assets.load_lake()
+
+        terrain.SurfaceCache = {}
+
+    def move_player(terrain, keys):
+        nerf = 1
+        keysPressed = [keys[pygame.K_d],keys[pygame.K_q],keys[pygame.K_z],keys[pygame.K_s]]
+        pressedKeysNumber = 0
+        for keyIsPressed in keysPressed:
+            if keyIsPressed:
+                pressedKeysNumber += 1
+        if pressedKeysNumber == 2:
+            nerf = (((terrain.PLAYER_SPEED**2)/2)**0.5)/terrain.PLAYER_SPEED 
+        if keys[pygame.K_d]:
+            terrain.x += terrain.PLAYER_SPEED*nerf
+        if keys[pygame.K_q]:
+            terrain.x -= terrain.PLAYER_SPEED*nerf
+        if keys[pygame.K_z]:
+            terrain.y -= terrain.PLAYER_SPEED*nerf
+        if keys[pygame.K_s]:
+            terrain.y += terrain.PLAYER_SPEED*nerf
+        terrain.x,terrain.y = round(terrain.x),round(terrain.y)
+
+    def modify_tile(terrain, mousePos, newTileType):
+        tileX = (terrain.x + mousePos[0]) // terrain.TILE_SIZE
+        tileY = (terrain.y + mousePos[1]) // terrain.TILE_SIZE
+        if (tileX, tileY) in terrain.ModifiedTiles:
+            return 
+        elif newTileType == "farmland":
+            terrain.ModifiedTiles[(tileX, tileY)] = terrain.assets.Blocks.EveryBlock["farmland"]
+            if (tileX, tileY) in terrain.SurfaceCache:
+                del terrain.SurfaceCache[(tileX, tileY)]
+
+
+
+    def draw_terrain(terrain, screen):
+        biomeScale = 0.05
+
+        startScreenX = int(terrain.x // terrain.TILE_SIZE)
+        startScreenY = int(terrain.y // terrain.TILE_SIZE)
+        endScreenX = int((terrain.x + terrain.SCREEN_WIDTH) // terrain.TILE_SIZE) + 2
+        endScreenY = int((terrain.y + terrain.SCREEN_HEIGHT) // terrain.TILE_SIZE) + 3
+        
+        playerTileX = (terrain.x + terrain.SCREEN_WIDTH // 2) // terrain.TILE_SIZE
+        playerTileY = (terrain.y + terrain.SCREEN_HEIGHT // 2) // terrain.TILE_SIZE
+
+        mouseTileX = (terrain.x + pygame.mouse.get_pos()[0]) // terrain.TILE_SIZE
+        mouseTileY = (terrain.y + pygame.mouse.get_pos()[1]) // terrain.TILE_SIZE
+
+        for tileY in range(startScreenY, endScreenY):
+            for tileX in range(startScreenX, endScreenX):
+                drawX = tileX * terrain.TILE_SIZE - terrain.x
+                drawY = tileY * terrain.TILE_SIZE - terrain.y
+
+                terrain.draw_tile(biomeScale, tileX, tileY, drawX, drawY, screen)
+                if tileX == playerTileX and tileY == playerTileY:
+                    screen.blit(terrain.lightOverlay, (drawX, drawY))
+
+                if tileX == mouseTileX and tileY == mouseTileY:
+                    screen.blit(terrain.mouseHighlightOverlay, (drawX, drawY))
+                
+
+    def draw_tile(terrain, biomeScale, tileX, tileY, drawX, drawY, screen):
+        
+        # Check modified tiles
+        if (tileX, tileY) in terrain.ModifiedTiles:
+            screen.blit(terrain.ModifiedTiles[(tileX, tileY)], (drawX, drawY))
+            return 
+        
+        # Check cache 
+        if (tileX, tileY) in terrain.SurfaceCache:
+            cached = terrain.SurfaceCache[(tileX, tileY)]
+            if isinstance(cached, tuple):
+                surf, xOff, yOff = cached
+                screen.blit(surf, (drawX - xOff, drawY - yOff))
+            else:
+                screen.blit(cached, (drawX, drawY))
+            return
+
+        tileSurface = pygame.Surface((terrain.TILE_SIZE * 3, terrain.TILE_SIZE * 3), pygame.SRCALPHA)
+        baseDrawX = terrain.TILE_SIZE
+        baseDrawY = terrain.TILE_SIZE * 2
+
+        randomValueGrassWater = get_random_value(tileX, tileY, 174)
+        randomValueNatureTree = get_random_value(tileX, tileY, 194)
+
+
+        # Biome (grass type)
+        noiseValueBiome = terrain.tmpNoiseBiomes.noise2(tileX * biomeScale, tileY * biomeScale)
+        grassType = 0 if noiseValueBiome < -0.333 else (3 if noiseValueBiome < 0.333 else 6)
+
+
+        # Water
+        noiseValueWater = terrain.tmpNoiseWater.noise2(tileX * biomeScale * 2, tileY * biomeScale * 2)
+
+        if noiseValueWater < -0.6:
+            lakeType = 0 if randomValueGrassWater < 0.8 else (1 if randomValueGrassWater < 0.95 else 2)
+            tileSurface.blit(terrain.assets.LakeList[lakeType], (baseDrawX, baseDrawY))
+        else:
+
+            # Foliage
+            foliageType = 0 if randomValueGrassWater < 0.9 else (1 if randomValueGrassWater < 0.97 else 2)
+            tileSurface.blit(terrain.assets.GrassList[foliageType + grassType], (baseDrawX, baseDrawY))
+
+
+            # Nature and Trees
+            if grassType == 3:
+                if randomValueNatureTree < 0.01:
+                    natureChoice = terrain.assets.NatureList[int(randomValueGrassWater * len(terrain.assets.NatureList))]
+                    tileSurface.blit(natureChoice, (baseDrawX, baseDrawY))
+                elif randomValueNatureTree < 0.02:
+                    treeChoice = terrain.assets.TreesList[int(randomValueGrassWater * len(terrain.assets.TreesList))]
+                    tileSurface.blit(treeChoice, (baseDrawX - terrain.TILE_SIZE, baseDrawY - terrain.TILE_SIZE*2))
+                    
+        terrain.SurfaceCache[(tileX, tileY)] = (tileSurface, baseDrawX, baseDrawY)
+        screen.blit(tileSurface, (drawX - baseDrawX, drawY - baseDrawY))
