5671185d4cf69257413b12fea276dae79a8e10c3
Guillaume <guillaume2aix.dly@gmail.com>
2026-02-24 16:43:07 +0100

    idonu

--- src/miner.py
+++ src/miner.py
@@ -7,6 +7,8 @@ class MinerClass:
         miner.color = colorPatches[(x,y)]
         miner.storage = []
         miner.mineCooldown = 60
+        miner.outputcooldown = 30
+        miner.output = (miner.x+1,miner.y)
 
     def mine(miner):
         if len(miner.storage) < 10 and miner.mineCooldown == 0:
@@ -15,4 +17,8 @@ class MinerClass:
         else:
             miner.mine_cooldown -= 1
         
-    
+    def empty_storage(miner):
+        if miner.output is not None:
+            miner.outputcooldown -= 1 
+            #if miner.outputcooldown == 0:
+                
